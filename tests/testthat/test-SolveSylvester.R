91cbe89212d20baf8cac8239c77e4102187d4811
lingxuko <lingxuko@umich.edu>
2021-12-04 20:19:58 -0500

    test4

--- tests/testthat/test-SolveSylvester.R
+++ tests/testthat/test-SolveSylvester.R
@@ -5,6 +5,13 @@ test_that("compare matrices",{
   y <- matrix(c(1,0,1,0,1,0),3,2)
   expect_equal(as.vector(x), as.vector(y), tolerance=1e-10)})
 
+test_that("compare matrices",{
+  x <- SolveSylvester(matrix(c(1,0,0,1),2,2,byrow = TRUE),
+                      matrix(c(1,0,0,0,1,0,0,0,1),3,3,byrow = TRUE),
+                      matrix(c(1,1,1,1,1,1),2,3))
+  y <- matrix(c(0.5,0.5,0.5,0.5,0.5,0.5),3,2)
+  expect_equal(as.vector(x), as.vector(y), tolerance=1e-10)})
+
 
 test_that("compare matrices",{
   x <- SolveSylvester(matrix(c(1,0,0,0,1,0,0,0,1),3,3,byrow = TRUE),
